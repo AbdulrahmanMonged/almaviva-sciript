2fef5203b962fe0750f8078771482f4e87471b6f
AbdulrahmanMonged <abdoomonged231@gmail.com>
2024-04-20 01:53:47 +0200

    go

--- cogs/almaviva_script.py
+++ cogs/almaviva_script.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 from awesometkinter.bidirender import render_text
 import threading
 from . import sheet_management
--- cogs/secretvars2.py
+++
@@ -1,3 +0,0 @@
-URI = "postgresql://AbdulrahmanMonged:YdDtzrB46JCU@ep-lucky-tree-44958310.eu-central-1.aws.neon.tech/almaviva_db?sslmode=require"
-AUTH_CODE = "kEqxp5oUpqdszAHPEB3pZkO3hELISEGCiSbd7HmeqpoYo5rr4BLUcMIFTTOYr6WI"
-ID : int
